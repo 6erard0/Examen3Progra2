1760f6ac9db3625ce82dd8813c8b2cc7525fc59b
Gerardo N U <113747797+6erard0@users.noreply.github.com>
2023-12-15 21:09:19 -0600

    Agregar archivos de proyecto.

---
+++ Examen3Progra2/DBconexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Examen3Progra2
+{
+    public class DBconexion
+    {
+        public static SqlConnection obtenerConexion()
+        {
+            string s = ConfigurationManager.ConnectionStrings["EncuestaConnectionString"].ConnectionString;
+            SqlConnection conexion = new SqlConnection(s);
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
